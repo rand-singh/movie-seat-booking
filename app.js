46b1d8f7ae8db60bbf3498eeaec5276a22732e8a
Randeep Singh <randeeprattan90@gmail.com>
2020-05-21 20:22:22 +0100

    Select movie and seats from the UI

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("app js");
+const container = document.querySelector(".container"),
+  seats = document.querySelectorAll(".row .seat:not(.occupied)"),
+  count = document.getElementById("count"),
+  total = document.getElementById("total"),
+  movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value; // converts the string value to a number
+
+// update total and price
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected"),
+    selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// seat click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
