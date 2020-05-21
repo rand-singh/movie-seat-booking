0593171668ce3cdda7c00e58efd4599582b52435
Randeep Singh <randeeprattan90@gmail.com>
2020-05-21 21:15:28 +0100

    Save info to local storage

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ const container = document.querySelector(".container"),
 
 let ticketPrice = +movieSelect.value; // converts the string value to a number
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 // update total and price
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected"),
     selectedSeatsCount = selectedSeats.length;
+
+  // copy selected seats in arr
+  // map throgh array
+  // return a new array of indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -18,6 +31,7 @@ function updateSelectedCount() {
 // movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
